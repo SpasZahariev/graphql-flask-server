de9ff17b19743085ec04e7298f75b134a964fe44
Spas_Z <spaszahariev54@gmail.com>
2020-02-26 19:23:20 +0000

    made bash script generic so it can be run on windows docker too

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-cd /src
+cd src
 
-#only works on heroku since it sets a PORT as an environment variable that I MUST work with
-gunicorn --bind 0.0.0.0:${PORT} main:app
+OPEN_PORT=${PORT}
+
+if [$OPEN_PORT -e ''];then 
+    OPEN_PORT='5000'
+fi
+
+
+gunicorn --bind 0.0.0.0:$OPEN_PORT main:app
 
 #use this for windows
 # gunicorn --bind 0.0.0.0:5000 main:app
